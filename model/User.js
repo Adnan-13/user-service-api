5327c7ca7e469317a68a5e90a8b637f427bbdfef
Adnan Bin Zahir <adnankp09@gmail.com>
2024-02-16 03:19:30 +0600

    🆕 feat: add password and validator

--- model/User.js
+++ model/User.js
@@ -1,15 +1,24 @@
 const mongoose = require('../config/db/db');
+const bcrypt = require('bcrypt');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'Please enter a name'],
     },
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'],
         unique: true,
         lowercase: true,
+        validate: [isEmail, 'Please enter a valid email'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password'],
+        minlength: [8, 'Minimum password length is 8 characters'],
+        select: false,
     },
     dob: Date,
     enabled: {
@@ -38,4 +47,14 @@ userSchema.pre('save', function (next) {
     next();
 });
 
-module.exports = mongoose.model('users', userSchema);
+userSchema.methods.comparePassword = function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+userSchema.pre('save', async function () {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+});
+
+module.exports = mongoose.model('user', userSchema);
